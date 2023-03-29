670b01cafa6aa0b777573392ca0304a462bda4ec
Jerem B <jrr87328@gmail.com>
2023-03-29 20:53:16 +0200

    creation of the home page and fixtures

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Trick;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    /**
+     * This function load initial data
+     *
+     * @param ObjectManager $manager
+     * @return void
+     */
+    public function load(ObjectManager $manager): void
+    {
+        $trick = new Trick();
+        $trick->setTricksGroup('Grabs');
+        $trick->setName('Mute');
+        $trick->setDescription('saisie de la carre frontside de la planche entre les deux pieds avec la main avant');
+        $trick->setPicture('images/mute.jpg');
+
+        $manager->persist($trick);
+
+        $trick = new Trick();
+        $trick->setTricksGroup('Grabs');
+        $trick->setName('Sad');
+        $trick->setDescription('saisie de la carre backside de la planche, entre les deux pieds, avec la main avant');
+
+        $manager->persist($trick);
+
+        $trick = new Trick();
+        $trick->setTricksGroup('Grabs');
+        $trick->setName('Indy');
+        $trick->setDescription('saisie de la carre frontside de la planche, entre les deux pieds, avec la main arrière');
+
+        $manager->persist($trick);
+
+        $trick = new Trick();
+        $trick->setTricksGroup('Grabs');
+        $trick->setName('Stalefish');
+        $trick->setDescription('saisie de la carre backside de la planche entre les deux pieds avec la main arrière');
+
+        $manager->persist($trick);
+
+        $trick = new Trick();
+        $trick->setTricksGroup('Grabs');
+        $trick->setName('Tail grab');
+        $trick->setDescription('saisie de la partie arrière de la planche, avec la main arrière');
+
+        $manager->persist($trick);
+
+        $trick = new Trick();
+        $trick->setTricksGroup('Grabs');
+        $trick->setName('Nose grab');
+        $trick->setDescription('saisie de la partie avant de la planche, avec la main avant');
+
+        $manager->persist($trick);
+
+        $trick = new Trick();
+        $trick->setTricksGroup('Grabs');
+        $trick->setName('Japan air');
+        $trick->setDescription('saisie de l\'avant de la planche, avec la main avant, du côté de la carre frontside');
+
+        $manager->persist($trick);
+
+        $trick = new Trick();
+        $trick->setTricksGroup('Grabs');
+        $trick->setName('Seat belt');
+        $trick->setDescription('saisie du carre frontside à l\'arrière avec la main avant');
+
+        $manager->persist($trick);
+
+        $trick = new Trick();
+        $trick->setTricksGroup('Flips');
+        $trick->setName('Front flip');
+        $trick->setDescription('rotation en avant');
+
+        $manager->persist($trick);
+
+        $trick = new Trick();
+        $trick->setTricksGroup('Flips');
+        $trick->setName('Back flip');
+        $trick->setDescription('rotation en arrière');
+
+        $manager->persist($trick);
+
+
+        $manager->flush();
+    }
+}
---
+++ src/Entity/Trick.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TrickRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
+
+
+#[UniqueEntity(['id', 'name'])]
+#[ORM\Entity(repositoryClass: TrickRepository::class)]
+
+class Trick
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 100)]
+    #[Assert\NotBlank]
+    #[Assert\Length(max: 100)]
+    private ?string $name = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    #[Assert\NotBlank]
+    private ?string $description = null;
+
+    #[ORM\Column(length: 50)]
+    #[Assert\NotBlank]
+    #[Assert\Length(max: 50)]
+    private ?string $tricksGroup = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    #[Assert\Length(max: 255)]
+    private ?string $picture = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    #[Assert\Length(max: 255)]
+    private ?string $video = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    /**
+     * DateTime constructor
+     */
+    public function __construct()
+    {
+        $this->createdAt = new \DateTimeImmutable;
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getTricksGroup(): ?string
+    {
+        return $this->tricksGroup;
+    }
+
+    public function setTricksGroup(string $tricksGroup): self
+    {
+        $this->tricksGroup = $tricksGroup;
+
+        return $this;
+    }
+
+    public function getPicture(): ?string
+    {
+        return $this->picture;
+    }
+
+    public function setPicture(?string $picture): self
+    {
+        $this->picture = $picture;
+
+        return $this;
+    }
+
+    public function getVideo(): ?string
+    {
+        return $this->video;
+    }
+
+    public function setVideo(?string $video): self
+    {
+        $this->video = $video;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+}
